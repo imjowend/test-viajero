68fdc6fc4c825a9a9963ce2738039531a5ec69b7
Joaquin Vasquez <joaquinvcestudio@gmail.com>
2026-07-05 05:23:19 -0500

    Conexion a vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://132.145.131.49:8082/api/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
