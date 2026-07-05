6c74a6740280f186b3b9666f49113c971ce697c5
Joaquin Vasquez <joaquinvcestudio@gmail.com>
2026-07-05 05:32:48 -0500

    rewrite de rutas vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://132.145.131.49:8082/api/:path*',
+        destination: 'http://132.145.131.49:8082/:path*',
       },
     ]
   },
